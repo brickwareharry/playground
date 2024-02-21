7ad8a61977339e6c36b251d39430d7d6bfa38d7f
harrykhlo <harrykhlo@gmail.com>
2024-02-22 05:38:07 +1300

    Add palindrome number code challenge

---
+++ palindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace palindrome;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int userInput = 121;
+        bool result = false;
+        string inputNum = Convert.ToString(userInput);
+        char[] inputCharArray = inputNum.ToCharArray();
+        Array.Reverse(inputCharArray);
+        string outputNum = new string(inputCharArray);
+        if (inputNum == outputNum)
+        {
+            result = true;
+        }
+        Console.WriteLine(result);
+    }
+}
